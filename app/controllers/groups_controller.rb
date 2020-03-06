4c1d4b73f382ce1e9cdb294518e84da36d8bcacb
kashimaraiki <raiki3290@gmail.com>
2020-03-06 14:29:00 +0900

    messege_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,7 @@
 class GroupsController < ApplicationController
-
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,13 +17,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path
+      redirect_to root_path, notice: 'グループを更新しました'
     else
       render :edit
     end
