ef44d78362617bbb56c6ba9e7bc3fd6ae7560523
kashimaraiki <raiki3290@gmail.com>
2020-06-06 21:48:36 +0900

    database new

--- db/schema.rb
+++ db/schema.rb
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 20200305112736) do
     t.string   "image"
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["group_id"], name: "fk_rails_841b0ae6ac", using: :btree
+    t.index ["user_id"], name: "fk_rails_273a25a7a6", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
